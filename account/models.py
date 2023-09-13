434c15b878c54e6397e8fb54707bdc34bc75985d
catalunha <catalunha.mj@gmail.com>
2023-09-13 14:51:45 -0300

    first commit

---
+++ account/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from account.managers import AccountManager
+import uuid
+
+
+class AccountModel(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+    )
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = AccountManager()
+
+    def __str__(self):
+        return self.email
